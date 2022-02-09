3aefe88675cd50482d67767ef349fcb9e49600e6
phucmt <mbat243@gmail.com>
2022-02-09 15:00:53 +0700

    check user token exist in app when launch app

--- app/src/main/java/com/nagaja/the330/MainActivity.kt
+++ app/src/main/java/com/nagaja/the330/MainActivity.kt
@@ -1,32 +1,42 @@
 package com.nagaja.the330
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.gson.Gson
 import com.nagaja.the330.base.ViewController
+import com.nagaja.the330.base.ViewModelFactory
+import com.nagaja.the330.data.DataStorePref
+import com.nagaja.the330.data.dataStore
+import com.nagaja.the330.model.AuthTokenModel
+import com.nagaja.the330.network.ApiService
+import com.nagaja.the330.network.RetrofitBuilder
 import com.nagaja.the330.utils.ScreenId
+import com.nagaja.the330.view.general.GeneralViewModel
 import com.nagaja.the330.view.login.LoginFragment
-import com.nagaja.the330.view.permission.PermissionFragment
-import com.nagaja.the330.view.signupinfo.SignupInfoFragment
-import com.nagaja.the330.view.term_sns.TermSNSFragment
+import com.nagaja.the330.view.main.MainFragment
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewController: ViewController
+    lateinit var generalViewModel: GeneralViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewController = ViewController(R.id.flContainer, supportFragmentManager, this)
+        generalViewModel = getViewModelProvider()[GeneralViewModel::class.java]
 
-        viewController.pushFragment(
-            ScreenId.SCREEN_LOGIN,
-            LoginFragment.newInstance()
-        )
+        checkOrFakeToken()
 
         WindowCompat.setDecorFitsSystemWindows(window, true)
-        ViewCompat.getWindowInsetsController(window.decorView).let { controller->
+        ViewCompat.getWindowInsetsController(window.decorView).let { controller ->
             controller?.isAppearanceLightStatusBars = true
             controller?.show(WindowInsetsCompat.Type.systemBars())
             controller?.systemBarsBehavior =
@@ -38,5 +48,67 @@ class MainActivity : AppCompatActivity() {
 //            controller.systemBarsBehavior =
 //                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 //        }
+
+        generalViewModel.callbackUserDetails.observe(this) {
+            if (it.agreePolicy == true) {
+                DataStorePref(this).setUserDetail(it)
+                viewController.pushFragment(
+                    ScreenId.SCREEN_MAIN,
+                    MainFragment.newInstance()
+                )
+            } else {
+                viewController.pushFragment(
+                    ScreenId.SCREEN_LOGIN,
+                    LoginFragment.newInstance()
+                )
+            }
+        }
+        generalViewModel.callbackUserFail.observe(this) {
+            viewController.pushFragment(
+                ScreenId.SCREEN_LOGIN,
+                LoginFragment.newInstance()
+            )
+        }
+    }
+
+    fun formatToken(token: String?): String {
+        if (TextUtils.isEmpty(token)) {
+//            showMess("Token Empty!")
+            return ""
+        }
+        return "Bearer $token"
+    }
+
+    private fun getViewModelProvider(): ViewModelProvider {
+        return ViewModelProvider(
+            this,
+            ViewModelFactory(
+                RetrofitBuilder.getInstance(this)
+                    ?.create(ApiService::class.java)!!
+            )
+        )
+    }
+
+    private fun checkOrFakeToken() {
+        CoroutineScope(Dispatchers.IO).launch {
+            var tokenModel: AuthTokenModel?
+            dataStore.data.map { get ->
+                get[DataStorePref.AUTH_TOKEN] ?: ""
+            }.collect {
+                tokenModel = Gson().fromJson(it, AuthTokenModel::class.java)
+                delay(1000)
+                withContext(Dispatchers.Main) {
+                    if (tokenModel == null) {
+                        viewController.pushFragment(
+                            ScreenId.SCREEN_LOGIN,
+                            LoginFragment.newInstance()
+                        )
+                    } else {
+                        val accessToken = formatToken(tokenModel!!.accessToken)
+                        generalViewModel.getUserDetails(accessToken)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/nagaja/the330/view/mypage/MyPageScreen.kt
+++ app/src/main/java/com/nagaja/the330/view/mypage/MyPageScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -26,6 +27,7 @@ import com.nagaja.the330.view.text14_222
 @Preview
 @Composable
 fun MyPageScreen() {
+    val context = LocalContext.current
     val clickFavorite: (()-> Unit) = {
         Log.e("PHUCDZ", "CLICK")
     }
